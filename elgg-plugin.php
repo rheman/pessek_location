c01e7f9c84b937da2da91d622380cd4902c7fdf2
Hermand PESSEK <pessek@bitbucket.org>
2021-10-27 15:45:27 -0400

    Release 4.0

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -5,6 +5,10 @@ use Pessek\PessekLocation\Bootstrap;
 $externaljs = dirname(__FILE__);
 
 return [
+        'plugin' => [
+                'version' => '4.0',
+		'name' => 'Google Maps Location for ELGG',
+        ],
 	'bootstrap' => \Pessek\PessekLocation\Bootstrap::class,
 	'views' => [
 		'default' => [
